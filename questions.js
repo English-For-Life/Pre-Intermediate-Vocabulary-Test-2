25a6f8d5a4eeea7786f4e74363d056c5173587a0
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-08 22:17:39 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"How did they ...... when they heard the price?",
+        options:["react", "knock", "discuss", "hate", "laugh"],
+        answer: 0
+    },
+    {
+        q:"It takes a lot of patience to ...... hyperactive children because of their low level of concentration.",
+        options:["describe", "teach", "discover", "joke with", "think about"],
+        answer: 1
+    },
+    {
+        q:"Don't ...... to call me if you need help.",
+        options:["dream", "shame", "hesitate", "repeat", "frighten"],
+        answer: 2
+    },
+    {
+        q:"The concert was called ...... because of the heavy rain.",
+        options:["out of", "aside", "down", "off", "out"],
+        answer: 3
+    },
+    {
+        q:"It is not often that we get a(n) ...... to meet a film star.",
+        options:["dream", "taste", "reality", "acceptance", "chance"],
+        answer: 4
+    },
+    {
+        q:"The pirates ...... the treasure on a desert island.",
+        options:["buried", "grilled", "forgave", "invented", "cooked"],
+        answer: 0
+    },
+    {
+        q:"Aren't you going to ...... me to your friend?",
+        options:["memorise", "introduce", "receive", "demonstrate", "joke with"],
+        answer: 1
+    },
+    {
+        q:"Plants absorb water from the soil through their ......   .",
+        options:["leaves", "branches", "roots", "flowers", "stems"],
+        answer: 2
+    },
+    {
+        q:"They didn't stay for supper. They weren't ......   .",
+        options:["sleepy", "early", "thirsty", "hungry", "poor"],
+        answer: 3
+    },
+    {
+        q:"He usually reads the sports ...... of the newspaper first.",
+        options:["tales", "literature", "chapter", "pictures", "section"],
+        answer: 4
+    }
+
+
+
+]
